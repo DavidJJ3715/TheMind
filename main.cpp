a61c759b2edbca179831e5f9d291cb9402ae8f74
DavidJJ3715 <davidjj3715@gmail.com>
2024-12-22 22:26:44 -0500

    Can now quit the game using a pause menu

--- main.cpp
+++ main.cpp
@@ -9,6 +9,37 @@ int main() {
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     TTF_Font* font = TTF_OpenFont("DejaVuSans.ttf", 75);
 
+    int frameTime = 0;
+    Uint64 frameStart = 0;
+    bool running = true;
+
+    while(running) {
+        frameStart = SDL_GetTicks64();
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            switch(event.type) {
+                case SDL_QUIT: {
+                    running = false;
+                    break;
+                }
+                case SDL_KEYDOWN: {
+                    if(event.key.keysym.sym == SDLK_ESCAPE) {
+                        running = selectionMenu(renderer, font, drawPause);
+                    }
+                    break;
+                }
+            }
+        }
+        
+        SDL_SetRenderDrawColor(renderer,0,0,0,0);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        frameTime = SDL_GetTicks64() - frameStart;
+        if(frameDelay > frameTime) {
+            SDL_Delay(frameDelay - frameTime);
+        }
+    }
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     TTF_CloseFont(font);
